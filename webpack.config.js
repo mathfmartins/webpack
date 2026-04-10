592f1fe2e918cbb6701b4693271ab60cc3e7ddb0
Matheus Martins <math.fmartins42@gmail.com>
2026-04-10 12:01:45 -0300

    chore: add css minimizer

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
     entry: './app/src/js/app.js',
@@ -13,6 +14,10 @@ module.exports = {
         rules: [
             { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader']}
         ]
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new CssMinimizerWebpackPlugin()]
     },
     plugins: [new HtmlWebpackPlugin({
         template: './app/src/app.html',
